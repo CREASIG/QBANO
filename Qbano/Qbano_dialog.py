97a7151c57696a9a39a61586e3675304a477dd51
CREASIG <vdev@free.fr>
2015-09-22 15:54:06 +0200

    re indentation pour passage à eclipse

--- Qbano/Qbano_dialog.py
+++ Qbano/Qbano_dialog.py
@@ -48,7 +48,7 @@ class QbanoDialog(QtGui.QDialog, FORM_CLASS):
         # Set up the user interface from Designer.
         # After setupUI you can access any designer object by doing
         # self.<objectname>, and you can use autoconnect slots - see
-       # http://qt-project.org/doc/qt-4.8/designe-using-a-ui-file.html
+        # http://qt-project.org/doc/qt-4.8/designe-using-a-ui-file.html
         # #widgets-and-dialogs-with-auto-connect
         self.setupUi(self)
 
@@ -56,8 +56,8 @@ class QbanoDialog(QtGui.QDialog, FORM_CLASS):
         self._listecouches.clear()
         layers = QgsMapLayerRegistry.instance().mapLayers()
         for name, layer in layers.iteritems():
-           #if layer.geometryType() == QGis.NoGeometry :
-           self._listecouches.addItem(name)
+            #if layer.geometryType() == QGis.NoGeometry :
+            self._listecouches.addItem(name)
 
     def remplirChampAdresse(self, valeur):
         self._champadresse.clear()
@@ -65,13 +65,13 @@ class QbanoDialog(QtGui.QDialog, FORM_CLASS):
         if valeur != None:
             for name, layer in layers.iteritems():
                 if name == valeur:
-                   for field in layer.pendingFields():
+                    for field in layer.pendingFields():
                         self._champadresse.addItem(field.name())
 
     def deboguer(self, texte):
-      #logging.basicConfig(filename='myapp.log', level=logging.INFO)
-      #logging.info(texte)
-      texte=""
+        #logging.basicConfig(filename='myapp.log', level=logging.INFO)
+        #logging.info(texte)
+        pass
       
 
     def geocoder(self):
@@ -84,86 +84,87 @@ class QbanoDialog(QtGui.QDialog, FORM_CLASS):
                 
                 #Creation de la couche
                 vl = QgsVectorLayer("Point?crs=EPSG:4326", "temporary_points", "memory")
-		pr = vl.dataProvider()
+                pr = vl.dataProvider()
 
-		# Enter editing mode
-		vl.startEditing()
-		titre=[];
+                # Enter editing mode
+                vl.startEditing()
+                titre=[];
                 for att in layer.pendingAllAttributesList():
-			titre.append( QgsField(layer.attributeDisplayName(att), QVariant.String))
-		titre.append( QgsField("adresse_trouvee", QVariant.String))
-		titre.append( QgsField("score", QVariant.String))
-		titre.append( QgsField("type", QVariant.String))
-		pr.addAttributes(titre);
-		vl.commitChanges()
+                    titre.append( QgsField(layer.attributeDisplayName(att), QVariant.String))
+                titre.append( QgsField("adresse_trouvee", QVariant.String))
+                titre.append( QgsField("score", QVariant.String))
+                titre.append( QgsField("type", QVariant.String))
+                pr.addAttributes(titre);
+                vl.commitChanges()
                 self._progression.setMinimum(0)
                 
                 j=0
                 for i in selected_features:
-		    j=j+1;
+                    j=j+1;
                 
                 self._progression.setMaximum(j)
                 
                 selected_features = layer.getFeatures()
                 j=0
                 for i in selected_features:
-		    j=j+1;
-		    self.deboguer(j)
-		    self._progression.setValue(j)
-		    QtGui.qApp.processEvents()
-		    # self.deboguer("1")
-		    coordonnees={}
-		    coordonnees["adresse"] = ''
-		    coordonnees["lon"] = 0
-		    coordonnees["lat"] = 0
-		    coordonnees["score"] = 0
-		    coordonnees["type"] = 'empty'
-		    if i.attribute(champadresse) is not None and i.attribute(champadresse) and isinstance(i.attribute(champadresse), unicode) :
-			coordonnees = self.coordonnees(i.attribute(champadresse))
-		    if coordonnees =={}:
-		      break
-		    #self.deboguer("2")
-		    # self.deboguer(coordonnees)
+                    j=j+1;
+                    self.deboguer(j)
+                    self._progression.setValue(j)
+                    QtGui.qApp.processEvents()
+                    # self.deboguer("1")
+                    coordonnees={}
+                    coordonnees["adresse"] = ''
+                    coordonnees["lon"] = 0
+                    coordonnees["lat"] = 0
+                    coordonnees["score"] = 0
+                    coordonnees["type"] = 'empty'
+                    if i.attribute(champadresse) is not None and i.attribute(champadresse) and isinstance(i.attribute(champadresse), unicode) :
+                        coordonnees = self.coordonnees(i.attribute(champadresse))
+                    if coordonnees =={}:
+                        break
+                    #self.deboguer("2")
+                    # self.deboguer(coordonnees)
                     fet = QgsFeature()
-		    if coordonnees != {}:
-			fet.setGeometry( QgsGeometry.fromPoint(QgsPoint(coordonnees['lon'],coordonnees['lat']))) 
+                    if coordonnees != {}:
+                        fet.setGeometry( QgsGeometry.fromPoint(QgsPoint(coordonnees['lon'],coordonnees['lat']))) 
 
-		    attributs = []
-		    for att in layer.pendingAllAttributesList():
-			attributs.append(i.attribute(layer.attributeDisplayName(att)))
-		    if coordonnees != {}:
-			attributs.append(coordonnees['adresse'])
-			attributs.append(str(coordonnees['score']))
-			attributs.append(str(coordonnees['type']))
-		    fet.setAttributes(attributs)
-		    pr.addFeatures( [ fet ] )
-		# Commit changes
+                    attributs = []
+                    for att in layer.pendingAllAttributesList():
+                        attributs.append(i.attribute(layer.attributeDisplayName(att)))
+                    if coordonnees != {}:
+                        attributs.append(coordonnees['adresse'])
+                        attributs.append(str(coordonnees['score']))
+                        attributs.append(str(coordonnees['type']))
+                    fet.setAttributes(attributs)
+                    pr.addFeatures( [ fet ] )
+                    # Commit changes
 
-		vl.commitChanges()
+                vl.commitChanges()
                 
-		QgsMapLayerRegistry.instance().addMapLayer(vl)     
+                QgsMapLayerRegistry.instance().addMapLayer(vl)     
 
     def coordonnees(self,adresse):
 
-	try:
-	  param = {'wt':'json','rows':2,'fl':'score,*'}
-	  param['q']=unicodedata.normalize('NFD',adresse).encode('ascii','ignore')
-	  url = urllib.urlencode(param)
-	  donnee = urllib2.urlopen('http://api-adresse.data.gouv.fr/search/?' + url+'&limit=1')
-	  # self.deboguer(donnee)
-	  data = json.load(donnee)
-	  self.deboguer(data)
-	  retour = {}
-	  if len(data['features']) > 0 :
-            retour["adresse"] = data['features'][0]['properties']['label']
-            retour["lon"] = data['features'][0]['geometry']['coordinates'][0]
-            retour["lat"] = data['features'][0]['geometry']['coordinates'][1]
-            retour["score"] = data['features'][0]['properties']['score']
-            retour["type"] = data['features'][0]['properties']['type']
-	except  urllib2.HTTPError as e:
-	    QMessageBox.critical(self, QtGui.QApplication.translate("QBAN(O)", "QBAN(O)"), QtGui.QApplication.translate("Internetconnexionerror", "Internet connexion error"), QMessageBox.Ok)
-	    retour = {}
-	except  urllib2.URLError as e:
-	    QMessageBox.critical(self, QtGui.QApplication.translate("QBAN(O)", "QBAN(O)"), QtGui.QApplication.translate("Internetconnexionerror", "Internet connexion error"), QMessageBox.Ok)
-	    retour = {}
-	return retour
+        retour = {}
+        try:
+            param = {'wt':'json','rows':2,'fl':'score,*'}
+            param['q']=unicodedata.normalize('NFD',adresse).encode('ascii','ignore')
+            url = urllib.urlencode(param)
+            donnee = urllib2.urlopen('http://api-adresse.data.gouv.fr/search/?' + url+'&limit=1')
+            print( donnee)
+            # self.deboguer(donnee)
+            data = json.load(donnee)
+            self.deboguer(data)
+            if len(data['features']) > 0 :
+                retour["adresse"] = data['features'][0]['properties']['label']
+                retour["lon"] = data['features'][0]['geometry']['coordinates'][0]
+                retour["lat"] = data['features'][0]['geometry']['coordinates'][1]
+                retour["score"] = data['features'][0]['properties']['score']
+                retour["type"] = data['features'][0]['properties']['type']
+        except  urllib2.HTTPError as e:
+            QMessageBox.critical(self, QtGui.QApplication.translate("QBAN(O)", "QBAN(O)"), QtGui.QApplication.translate("Internetconnexionerror", "Internet connexion error"), QMessageBox.Ok)
+            retour = {}
+        except  urllib2.URLError as e:
+            QMessageBox.critical(self, QtGui.QApplication.translate("QBAN(O)", "QBAN(O)"), QtGui.QApplication.translate("Internetconnexionerror", "Internet connexion error"), QMessageBox.Ok)
+            retour = {}
+        return retour
